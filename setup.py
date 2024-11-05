8dcc3a951f818d6603453c7ce932e8cc33e218df
Jonathan Santana Silva <jonathan-works@hotmail.com>
2024-11-05 17:52:40 -0400

    chore: mudaca de ordem das pastas

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="etlspark",
-    version="0.1",
+    version="1.0.0.1",
     packages=find_packages(),
     install_requires=[],
     description="Lib etl",
